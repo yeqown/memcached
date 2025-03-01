d31e371ee1f37f3eee1f767e6c36adfe604502f2
yeqown <yeqown@gmail.com>
2025-03-01 08:42:04 +0800

    feat(client): support resolve tcp6 not only url address format

--- client_cluster_test.go
+++ client_cluster_test.go
@@ -29,52 +29,109 @@ func Test_defaultResolver_Resolve(t *testing.T) {
 	}
 }
 
-func Test_defaultResolver_Resolve_error(t *testing.T) {
-	tests := []struct {
-		name string
+func Test_defaultResolver_Resolve_multiType(t *testing.T) {
+	type args struct {
 		addr string
+	}
 
-		wantErr       bool
-		wantAddrCount int
-		wantAddresses map[string]struct{}
+	tests := []struct {
+		name     string
+		args     args
+		wantErr  bool
+		wantAddr Addr
 	}{
 		{
-			name:    "case1: empty address",
-			addr:    "",
-			wantErr: true,
-		},
-		{
-			name:          "case2: invalid format address",
-			addr:          "localhost:11211 , localhost:11212, localhost:11213,",
-			wantErr:       false,
-			wantAddrCount: 3,
-			wantAddresses: map[string]struct{}{
-				"localhost:11211": {},
-				"localhost:11212": {},
-				"localhost:11213": {},
+			name: "case1: v4",
+			args: args{
+				addr: "localhost:11211",
+			},
+			wantErr: false,
+			wantAddr: Addr{
+				Network:  "tcp",
+				Address:  "localhost:11211",
+				Priority: 0,
+				metadata: map[string]any{},
 			},
 		},
 		{
-			name:    "case3: illegal format address",
-			addr:    ":11211",
+			name: "case1: v4 with domain host",
+			args: args{
+				addr: "memcached.example.com:11211",
+			},
+			wantErr: false,
+			wantAddr: Addr{
+				Network:  "tcp",
+				Address:  "memcached.example.com:11211",
+				Priority: 0,
+				metadata: map[string]any{},
+			},
+		},
+		{
+			name: "case2: ip v6",
+			args: args{
+				addr: "[::1]:11211",
+			},
+			wantErr: false,
+			wantAddr: Addr{
+				Network:  "tcp",
+				Address:  "[::1]:11211",
+				Priority: 0,
+				metadata: map[string]any{},
+			},
+		},
+		{
+			name: "case3: unix socket, not supported yet",
+			args: args{
+				addr: "unix:///tmp/memcached.sock",
+			},
 			wantErr: true,
+			wantAddr: Addr{
+				Network:  "unix",
+				Address:  "/tmp/memcached.sock",
+				Priority: 0,
+				metadata: map[string]any{},
+			},
+		},
+		{
+			name: "case4: invalid address",
+			args: args{
+				addr: "invalid_address",
+			},
+			wantErr: true,
+		},
+		{
+			name: "case5: empty address",
+			args: args{
+				addr: "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "case6: multiple addresses",
+			args: args{
+				addr: "localhost:11211,localhost:11212,localhost:11213",
+			},
+			wantErr: false,
+			wantAddr: Addr{
+				Network:  "tcp",
+				Address:  "localhost:11211",
+				Priority: 0,
+				metadata: map[string]any{},
+			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			addrs, err := newDefaultResolver().Resolve(tt.addr)
+			addrs, err := newDefaultResolver().Resolve(tt.args.addr)
 			if tt.wantErr {
 				assert.Error(t, err)
 				return
 			}
 
 			assert.NoError(t, err)
-			assert.Equal(t, tt.wantAddrCount, len(addrs))
-
-			for _, addr := range addrs {
-				assert.Contains(t, tt.wantAddresses, addr.Address)
-			}
+			assert.GreaterOrEqual(t, len(addrs), 1)
+			assert.Equal(t, tt.wantAddr, *addrs[0])
 		})
 	}
 }
